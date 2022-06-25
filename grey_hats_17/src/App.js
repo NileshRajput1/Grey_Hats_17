ae36aa0c44ef9ec485fcfb1f76377c7510ae5f68
Alik Kumar Ghosh <33929979+Alik-Kumar-Ghosh@users.noreply.github.com>
2022-06-26 00:02:22 +0530

    Add files via upload

--- grey_hats_17/src/App.js
+++ grey_hats_17/src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
-
+import Navbar from './Components/Navbar';
+import Center from './Components/Center';
+import Card from './Components/Card';
+import Login from './Components/Login';
+import Pricing from './Components/Pricing';
+import CheckOut from './Components/CheckOut';
+import {BrowserRouter as Router, Route , Switch} from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-    
-    </div>
-  );
+  <Router>
+   
+    <Switch>
+      <Route exact path="/">
+      <Navbar/>
+    <Center/>
+    <Card />
+    <Pricing />
+      </Route>
+      <Route path="/login">
+        <Login/>
+      </Route>
+      <Route path="/checkout">
+        <CheckOut/>
+      </Route>
+    </Switch>
+  </Router>
+ );
 }
 
 export default App;
---
+++ grey_hats_17/src/Components/Login.js
@@ -0,0 +1,10 @@
+function Login() {
+    return (
+    <div>
+        <h1>Hii</h1>
+    </div>
+        );
+    }
+    
+  export default Login;
+    
