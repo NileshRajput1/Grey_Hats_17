1cbb073eb435ef7f46df8e1342154574e92d0552
Alik Kumar Ghosh <33929979+Alik-Kumar-Ghosh@users.noreply.github.com>
2022-06-25 21:19:17 +0530

    Add files via upload

--- grey_hats_17/src/App.js
+++ grey_hats_17/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import Center from './Components/Center';
+import Card from './Components/Card';
 function App() {
   return (
   <div>
     <Navbar/>
     <Center/>
+    <Card />
   </div>
  );
 }
